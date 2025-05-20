7f1ad5b70e699d27611047dd7eb33acedc18b865
yavuzsyl <yavuzsyl@gmail.com>
2025-05-20 23:47:38 +0300

    added catalog service & hosted postgres in apphost

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -2,4 +2,21 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 //projects & cloud-native backing services
 
+
+//backing services
+var postgres = builder
+    .AddPostgres("postgres") //container
+    .WithPgAdmin() //ui
+    .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent); 
+
+var catalogDb = postgres.AddDatabase(name: "catalogdb"); //only for referencing, we still need to create db 
+
+//projects
+var catalog = builder
+    .AddProject<Projects.Catalog>("catalog")
+    .WithReference(catalogDb)//injects cs to environment
+    .WaitFor(catalogDb);
+
+
 builder.Build().Run();
