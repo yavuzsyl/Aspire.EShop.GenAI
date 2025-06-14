406018a9a5cc2d12da209247f5e523103f7da05a
yavuzsyl <yavuzsyl@gmail.com>
2025-06-15 01:47:26 +0300

    support endpoint ui integration

--- WebApp/ApiClients/CatalogApiClient.cs
+++ WebApp/ApiClients/CatalogApiClient.cs
@@ -15,4 +15,10 @@ public class CatalogApiClient(HttpClient httpClient)
         var response = await httpClient.GetFromJsonAsync<Product>($"/products/{id}");
         return response!;
     }
+
+    public async Task<string> SupportChat(string query)
+    {
+        var response = await httpClient.GetFromJsonAsync<string>($"/products/support/{query}");
+        return response!;
+    }
 }
